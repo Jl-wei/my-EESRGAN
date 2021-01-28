bb41d7e387f391353faf821e2447bfec9f1f0b1a
Jialiang WEI <0301jlwei@gmail.com>
2021-01-28 21:11:00 +0100

    add VGGFeatureExtractor

---
+++ models/vgg_feature_extractor.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+import torch.nn as nn
+
+
+class VGGFeatureExtractor(nn.Module):
+    def __init__(self, feature_layer=34, use_bn=False, use_input_norm=False, # data already normalized, need checking
+                 device=torch.device('cpu')):
+        super(VGGFeatureExtractor, self).__init__()
+        self.use_input_norm = use_input_norm
+        if use_bn:
+            model = torchvision.models.vgg19_bn(pretrained=True)
+        else:
+            model = torchvision.models.vgg19(pretrained=True)
+        if self.use_input_norm:
+            mean = torch.Tensor([0.3442, 0.3708, 0.3476]).view(1, 3, 1, 1).to(device) # for cowc dataset
+            # [0.485 - 1, 0.456 - 1, 0.406 - 1] if input in range [-1, 1]
+            std = torch.Tensor([0.1232, 0.1230, 0.1284]).view(1, 3, 1, 1).to(device )# for cowc dataset
+            # [0.229 * 2, 0.224 * 2, 0.225 * 2] if input in range [-1, 1]
+            self.register_buffer('mean', mean)
+            self.register_buffer('std', std)
+        self.features = nn.Sequential(*list(model.features.children())[:(feature_layer + 1)])
+        # No need to BP to variable
+        for k, v in self.features.named_parameters():
+            v.requires_grad = False
+
+    def forward(self, x):
+        # Assume input range is [0, 1]
+        if self.use_input_norm:
+            x = (x - self.mean) / self.std
+        output = self.features(x)
+        return output
