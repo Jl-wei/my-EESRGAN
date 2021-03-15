c9c097148ee2d1f0266dffb8b68d8aaa46189614
Jialiang Wei <jialiang.wei@outlook.com>
2021-03-15 19:44:34 +0000

    combine tow test files

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import numpy as np
 import sys
 import logging
 import os
+import shutil
 
 import data
 import trainers
 import utils
 
 '''
-nohup stdbuf -oL python test.py > ./saved/logs/test.log &
+nohup stdbuf -oL python test.py config_hripcb.json > ./saved_hripcb/logs/test.log &
+nohup stdbuf -oL python test.py config.json > ./saved/logs/test.log &
 '''
 
 # fix random seeds for reproducibility
@@ -48,7 +50,8 @@ def main(config):
 
 
 if __name__ == '__main__':
-    config = utils.read_json('./config.json')
+    config_path = sys.argv[1]
+    config = utils.read_json(config_path)
 
     utils.setup_logger('valid', config['logger']['path'], 'test', 
                     level=logging.INFO,
@@ -57,11 +60,18 @@ if __name__ == '__main__':
     # import pdb; pdb.set_trace()
 
     config['test']['save_img'] = True
+    shutil.rmtree(config['path']['valid_img'], ignore_errors=True)
 
-    config['name'] = "pixel-0.01-feature-1-210307-223340"
+    config['name'] = "pixel-0.01-feature-1-210315-100912"
     config['pretrained_models']['load'] = True
     config['pretrained_models']['G'] = os.path.join(config['pretrained_models']['path'], "{}_G.pth".format(config['name']))
     config['pretrained_models']['D'] = os.path.join(config['pretrained_models']['path'], "{}_D.pth".format(config['name']))
     config['pretrained_models']['FRCNN'] = os.path.join(config['pretrained_models']['path'], "{}_FRCNN.pth".format(config['name']))
+
+    # config['name'] = "pixel-0.99-feature-0.01-210301-104831"
+    # config['pretrained_models']['load'] = True
+    # config['pretrained_models']['G'] = os.path.join("saved/pretrained_models", "{}_G.pth".format(config['name']))
+    # config['pretrained_models']['D'] = os.path.join("saved/pretrained_models", "{}_D.pth".format(config['name']))
+    # config['pretrained_models']['FRCNN'] = os.path.join("saved/pretrained_models", "{}_FRCNN.pth".format(config['name']))
 
     main(config)
